aa9575bef978f49d7aae251479f91348cb387658
Daniel Montet <dmontetproff@gmail.com>
2024-08-22 11:59:30 +0300

    feat: revalidation webhook

--- src/app/api/rebuild-path/route.ts
+++ src/app/api/rebuild-path/route.ts
@@ -2,13 +2,17 @@
 import { revalidatePath } from "next/cache"
 
 export async function GET(request: Request) {
+	const path = new URL(request.url).searchParams.get("path")
+	if (!path) {
+		return Response.json({ error: "Path not provided" }, { status: 400 });
+	}
+
 	try {
-		const path = new URL(request.url).searchParams.get("path")
-		// console.log("path", path)
 		await revalidatePath(path as string)
-		return Response.json({ data: { success: true } })
+		return Response.json({ data: { success: true }, status: 200 })
 	} catch (error: any) {
-		return Response.json({ error: "Error rebuilding path" })
+		console.log("error", error)
+		return Response.json({ error, status: 500 });
 	}
 
 }
